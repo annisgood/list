0b068df977483cabfc29d783f236fd1cff724b4f
annisgood <98206319+annisgood@users.noreply.github.com>
2022-02-06 21:07:58 +0800

    word count

--- read.py
+++ read.py
@@ -7,17 +7,33 @@ with open ('reviews.txt','r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了,總共有' , len(data),'筆資料')
-sum_len = 0
-for d  in data:
-	sum_len = sum_len + len(d)
-print('平均是',sum_len/len(data))
+
+print(data[0])
 
 
-new = []
-for  d in data:
-	if len(d) <100:
-		new.append(d)
-print('一共有',len(new),'筆小於100')
-print(new[0])
-print(new[1])
+#文字記數
 
+wc = {}
+for d in data:
+	words = d.split (' ')
+	for word in words:
+		if word in wc:
+			wc[word] +=1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word =   input('請問要查甚麼字:')
+	if word == 'q':
+		break
+	if word in wc:
+		print (word, '出現的次數為', wc[word])
+	else:
+		print('這個字沒有出現過喔') 
+print('感謝使用')
